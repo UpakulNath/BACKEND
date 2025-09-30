b439143e5fb9867b26a25600349fdd11a0e74f41
Upakul <upakuln1@gmail.com>
2025-09-30 19:49:32 +0530

    Add resend email verification feature

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -7,6 +7,7 @@ import {
   forgotPasswordMailgenContent,
   sendEmail,
 } from "../utils/mail.js";
+import { hash } from "bcrypt";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -192,5 +193,38 @@ const verifyEmail = asyncHandler(async (req, res) => {
       "Email is verified",
     ),
   );
+});
+
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiErrors(404, "User doesnot exist");
+  }
+
+  if (user.isEmailVerified) {
+    throw new ApiErrors(409, "Email is already verified");
+  }
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/users/verify-emails/${unHashedToken}`,
+    ),
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Mail has been sent to your email ID"));
 });
 export { registerUser, login, logoutUser, getCurrentUser, verifyEmail };
